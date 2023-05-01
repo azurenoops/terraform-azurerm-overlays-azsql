b16d9fc161380681ad8406f433d809fecfbbc42d
John Spinella <jrspinella@users.noreply.github.com>
2023-05-01 09:59:48 -0400

    Updates

--- resources.sqlsvr.locks.tf
+++ resources.sqlsvr.locks.tf
@@ -15,7 +15,7 @@ resource "azurerm_management_lock" "primary_sql_level_lock" {
 resource "azurerm_management_lock" "secondary_sql_level_lock" {
   count      = var.enable_resource_locks && var.enable_failover_group ? 1 : 0
   name       = "${local.secondary_server_name}-${var.lock_level}-lock"
-  scope      = azurerm_mssql_server.secondary_sql.id
+  scope      = azurerm_mssql_server.secondary_sql.0.id
   lock_level = var.lock_level
   notes      = "Azure SQl Server Secondary '${local.secondary_server_name}' is locked with '${var.lock_level}' level."
 }
